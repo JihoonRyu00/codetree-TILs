6c6ab8b82870f572d964a2894f408a206a5fd2c2
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2024-03-25 19:26:05 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240325/방향에 맞춰 이동/move-in-direction.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define N 0
+#define E 1
+#define S 2
+#define W 3
+
+using namespace std;
+
+int main() {
+    
+    int dx[4]={0,1,0,-1};
+    int dy[4]={1,0,-1,0};
+    pair<int,int> cur={0,0};
+    int n;
+    cin>>n;
+    char dir=' ';
+    int dis, dirn;
+    for(int i=0; i<n;i++){
+        cin>>dir>>dis;
+        if(dir=='N') dirn=0;
+        else if(dir=='E') dirn=1;
+        else if(dir=='S') dirn=2;
+        else dirn=3;
+        cur.first += dx[dirn]*dis;
+        cur.second += dy[dirn]*dis;
+    }
+    cout<<cur.first<<" "<<cur.second;
+    return 0;
+}
