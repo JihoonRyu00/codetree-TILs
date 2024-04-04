520371ed86c06d63cae89a5b27ac83137e33a815
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2024-04-04 19:22:47 +0900

    Title: 피보나치 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240404/피보나치 수/fibonacci-number.cpp
+++ 240404/피보나치 수/fibonacci-number.cpp
@@ -6,7 +6,6 @@ int arr[46]={0,};
 int fib(int n){
     if(n==0) return arr[0]=0;
     else if(n==1) return arr[1]=1;
-    else if(n==2) return arr[2]=1;
     else{
         if(arr[n]==0){
             return arr[n]=fib(n-1)+fib(n-2);
