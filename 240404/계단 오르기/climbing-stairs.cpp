70dcc63a39a65286a4f432e78862f709b4b282a0
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2024-04-04 19:39:26 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240404/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001]={0,};
+
+int dp(int n){
+    if(n==0||n==1||n==2||n==3) return arr[n];
+    if(arr[n]==0) return arr[n]=(arr[n-2]+arr[n-3])%10007;
+    return arr[n];
+    
+}
+
+int main() {
+    int n;
+    cin>>n;
+    arr[0]=0;
+    arr[1]=0;
+    arr[2]=1;
+    arr[3]=1;
+    cout<<dp(n);
+    return 0;
+}
