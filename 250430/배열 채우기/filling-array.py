7c2049722aff278c90f9335693de9874d2fbb8ef
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-04-30 18:25:22 +0900

    Title: 배열 채우기, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/배열 채우기/filling-array.py
+++ 250430/배열 채우기/filling-array.py
@@ -1,5 +1,25 @@
 arr=list(map(int,input().split()))
-if arr[-1]==0:
-    arr.pop()
+ans=[]
 while arr:
-    print(arr.pop(),end=" ")
+    # if arr[-1]==0:
+    #     ans=[]
+    # else:
+    #     ans.append(arr.pop())
+    x=arr.pop()
+    if x==0:
+        ans=[]
+    else:
+        ans.append(x)
+    
+
+print(*ans)
+
+
+# arr=[]
+# for _ in range(10):
+#     x=int(input())
+#     if x==0:
+#         break
+#     arr.append(elem)
+# for i in range(len(arr)-1,-1,-1):
+#     print(arr[i],end=" ")
