a58c75e380f6269af23de0b18cca81f13e5de8c3
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-04-30 16:31:43 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250430/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+arr=[[0]*m for _ in range(n)]
+
+# Please write your code here.
+
+# A 65 Z 90
+# ascii to char => chr()
+# char to ascii => ord()
+dr=[0,1,0,-1]
+dc=[1,0,-1,0]
+
+cur_dir=0
+r=0
+c=0
+arr[0][0]='A'
+
+for i in range(1,n*m):
+    rr=r+dr[cur_dir]
+    cc=c+dc[cur_dir]
+    if rr<0 or rr>=n or cc<0 or cc>=n or arr[rr][cc]!=0:
+        cur_dir=(cur_dir+1)%4
+    r=r+dr[cur_dir]
+    c=c+dc[cur_dir]
+    arr[r][c]=chr(i%34+65)
+
+for i in range(n):
+    print(*arr[i])
