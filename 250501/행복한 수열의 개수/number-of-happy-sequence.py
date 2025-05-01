f91d315eae310e090fa1c5915a9a8e9f157de7ac
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-01 20:20:39 +0900

    Title: 행복한 수열의 개수, Time: 73ms, Memory: 17MB, Status: Passed - Codetree

--- 250501/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250501/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,41 +1,43 @@
-n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
-
-# Please write your code here.
-happy=0
-
-def is_rhappy(r):
-    stack=[]
-    stack.append(grid[r][0])
-    for i in range(1,n):
-        cur=grid[r][i]
-        if stack and stack[-1]!=cur:
-                stack=[]
-        stack.append(cur)
-        if len(stack)==m:
-            return True
-    return False
-
-def is_chappy(c):
-    stack=[]
-    stack.append(grid[0][c])
-    for i in range(1,n):
-        cur=grid[i][c]
-        if stack and stack[-1]!=cur:
-                stack=[]
-        stack.append(cur)
-        if len(stack)==m:
-            return True
-    return False
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+seq = [0 for _ in range(n)]
 
 
-if m==1:
-    print(2*n)
-else:
-    for i in range(n):
-        if is_chappy(i):
-            happy+=1
-        if is_rhappy(i):
-            happy+=1
+def is_happy_sequence():
+    # 주어진 seq가 행복한 수열인지 판단하는 함수입니다.
+    consecutive_count, max_ccnt = 1, 1
+    for i in range(1, n):
+        if seq[i - 1] == seq[i]:
+            consecutive_count += 1
+        else:
+            consecutive_count = 1
         
-    print(happy)
+        max_ccnt = max(max_ccnt, consecutive_count)
+    
+    # 최대로 연속한 회수가 m이상이면 true를 반환합니다. 
+    return max_ccnt >= m
+
+
+num_happy = 0
+
+# 먼저 가로로 행복한 수열의 수를 셉니다.
+for i in range(n):
+    seq = grid[i][:]
+
+    if is_happy_sequence():
+        num_happy += 1
+
+# 세로로 행복한 수열의 수를 셉니다.
+for j in range(n):
+    # 세로로 숫자들을 모아 새로운 수열을 만듭니다.
+    for i in range(n):
+        seq[i] = grid[i][j]
+
+    if is_happy_sequence():
+        num_happy += 1
+
+print(num_happy)
