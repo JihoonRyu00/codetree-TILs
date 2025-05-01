95684568c90771a8b0f910bd49580023df79028a
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-01 20:05:36 +0900

    Title: 컨베이어 벨트, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250501/컨베이어 벨트/conveyor-belt.py
+++ 250501/컨베이어 벨트/conveyor-belt.py
@@ -3,10 +3,22 @@ u = list(map(int, input().split()))
 d = list(map(int, input().split()))
 
 # Please write your code here.
-con=u+d
-k=t%(2*n)
-for i in range(n):
-    print(con[i-k],end=" ")
-print("")
-for i in range(n,2*n,1):
-    print(con[i-k],end=" ")
+# con=u+d
+# k=t%(2*n)
+# for i in range(n):
+#     print(con[i-k],end=" ")
+# print("")
+# for i in range(n,2*n,1):
+#     print(con[i-k],end=" ")
+
+for _ in range(t):
+    uu=u[-1]
+    dd=d[-1]
+    for i in range(-1,-n,-1):
+        u[i]=u[i-1]
+        d[i]=d[i-1]
+    u[0]=dd
+    d[0]=uu
+
+print(*u)
+print(*d)
