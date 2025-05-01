f3cd6ff5cc160f3fee72e0a207138c19a6b97134
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-01 19:41:44 +0900

    Title: 최고의 33위치, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250501/최고의 33위치/best-place-of-33.py
+++ 250501/최고의 33위치/best-place-of-33.py
@@ -2,21 +2,21 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-def is_in_grid(r,c):
-    return r>=0 and r<n and c>=0 and c<n
+# def is_in_grid(r,c):
+#     return r>=0 and r<n and c>=0 and c<n
 
 def get_adj(r,c):
     x=0
     for i in range(3):
         for j in range(3):
-            if is_in_grid(r+i-1,c+j-1) and grid[r+i-1][c+j-1]==1:
+            if grid[r+i-1][c+j-1]==1:
                 x+=1
     return x
 
 ans=0
 
-for i in range(n):
-    for j in range(n):
+for i in range(1,n-1):
+    for j in range(1,n-1):
         ans=max(ans,get_adj(i,j))
 
 print(ans)
