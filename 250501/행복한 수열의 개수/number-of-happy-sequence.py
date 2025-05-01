044227ea729a06586f54ae94b92edfc810693b5c
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-01 20:14:10 +0900

    Title: 행복한 수열의 개수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250501/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,37 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+happy=0
+
+def is_rhappy(r):
+    stack=[]
+    stack.append(grid[r][0])
+    for i in range(1,n):
+        cur=grid[r][i]
+        if stack and stack[-1]!=cur:
+                stack=[]
+        stack.append(cur)
+        if len(stack)==m:
+            return True
+    return False
+
+def is_chappy(c):
+    stack=[]
+    stack.append(grid[0][c])
+    for i in range(1,n):
+        cur=grid[i][c]
+        if stack and stack[-1]!=cur:
+                stack=[]
+        stack.append(cur)
+        if len(stack)==m:
+            return True
+    return False
+
+for i in range(n):
+    if is_chappy(i):
+        happy+=1
+    if is_rhappy(i):
+        happy+=1
+    
+print(happy)
