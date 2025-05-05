ef848b7d6796d82c2a28c8c14e3161fc1939510c
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-05 21:08:24 +0900

    Title: N개 중에 M개 뽑기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250505/N개 중에 M개 뽑기/n-choose-m.py
+++ 250505/N개 중에 M개 뽑기/n-choose-m.py
@@ -2,12 +2,12 @@ N, M = map(int, input().split())
 
 # Please write your code here.
 answer=[]
-def choose(curr_num,maxn):
+def choose(curr_num,maxn,r):
     if curr_num==M+1:
         print(*answer)
         return
-    for i in range(maxn+1,N+1):
+    for i in range(maxn+1,N+1-r+1):
         answer.append(i)
-        choose(curr_num+1,i)
+        choose(curr_num+1,i,r-1)
         answer.pop()
-choose(1,0)
+choose(1,0,M)
