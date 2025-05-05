762183ffe3300ca8f8c641c14b84418764ab929a
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-05 20:33:50 +0900

    Title: 떨어지는 1자 블록, Time: 59ms, Memory: 16MB, Status: Passed - Codetree

--- 250505/떨어지는 1자 블록/falling-horizontal-block.py
+++ 250505/떨어지는 1자 블록/falling-horizontal-block.py
@@ -3,7 +3,7 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 k=k-1
-maxh=n-1
+maxh=n
 for c in range(k,k+m):
     for r in range(n):
         if grid[r][c]==1:
