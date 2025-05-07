f99816b0958c92bccec63d5bb59c5985616d3226
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-07 19:34:26 +0900

    Title: 그래프 탐색, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250507/그래프 탐색/graph-traversal.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+visit=[False]*(n+1)
+
+edge_list={}
+for edge in edges:
+    if edge[0] in edge_list:
+        edge_list[edge[0]].append(edge[1])
+    else:
+        edge_list[edge[0]]=[edge[1]]
+    if edge[1] in edge_list:
+        edge_list[edge[1]].append(edge[0])
+    else:
+        edge_list[edge[1]]=[edge[0]]
+
+visit[1]=True
+def bfs(node):
+    count=1
+    next_nodes=edge_list[node]
+    for next_node in next_nodes:
+        if not visit[next_node]:
+            visit[next_node]=True
+            count+=bfs(next_node)
+    return count
+
+print(bfs(1)-1)
