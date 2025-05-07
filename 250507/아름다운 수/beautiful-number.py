1eaaad93ef0cf2b45e4dcca2287ca95f5304bc17
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-07 18:40:31 +0900

    Title: 아름다운 수, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250507/아름다운 수/beautiful-number.py
+++ 250507/아름다운 수/beautiful-number.py
@@ -5,8 +5,8 @@ def beut(r):
     count=0
     if r==0:
         return 1
-    for i in range(1,r+1):
+    rr=min(r+1,5)
+    for i in range(1,rr):
         count+=beut(r-i)
     return count
-
 print(beut(n))
