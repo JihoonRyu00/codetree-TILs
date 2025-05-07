311d76dcfc44d7ccc3f7bfea2a84eaf40255311d
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2025-05-07 19:56:48 +0900

    Title: 그래프 탐색, Time: 96ms, Memory: 20MB, Status: Passed - Codetree

--- 250507/그래프 탐색/graph-traversal.py
+++ 250507/그래프 탐색/graph-traversal.py
@@ -6,23 +6,17 @@ visit=[False]*(n+1)
 
 edge_list={i:[] for i in range(1,n+1)}
 for edge in edges:
-    if edge[0] in edge_list:
-        edge_list[edge[0]].append(edge[1])
-    else:
-        edge_list[edge[0]]=[edge[1]]
-    if edge[1] in edge_list:
-        edge_list[edge[1]].append(edge[0])
-    else:
-        edge_list[edge[1]]=[edge[0]]
+    edge_list[edge[0]].append(edge[1])
+    edge_list[edge[1]].append(edge[0])
 
 visit[1]=True
-def bfs(node):
+def dfs(node):
     count=1
     next_nodes=edge_list[node]
     for next_node in next_nodes:
         if not visit[next_node]:
             visit[next_node]=True
-            count+=bfs(next_node)
+            count+=dfs(next_node)
     return count
 
-print(bfs(1)-1)
+print(dfs(1)-1)
